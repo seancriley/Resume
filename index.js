218bef7602189e3da311135ad957d32bd2ac2a42
Sean Riley <seancriley@earthlink.net>
2020-05-05 09:15:21 -0700

    initial

---
+++ index.js
@@ -0,0 +1,12 @@
+#! /usr/bin/env Node
+const fs = require('fs');
+
+fs.readFile(__dirname + '/info.json', 'utf8', function (err, data) {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log(data);
+		return data;
+	}
+});
+
